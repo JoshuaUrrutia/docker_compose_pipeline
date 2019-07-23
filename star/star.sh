014dc0c9395fb89632109c9f47fbc32cff84bfae
urrutia <jurrutia@tacc.utexas.edu>
2019-07-23 00:20:23 -0500

    namespacing alignment directories

--- star/star.sh
+++ star/star.sh
@@ -3,37 +3,41 @@ set -e
 
 R1=${1}
 R2=${2}
-outfile=${R1%_R1*}
+outdir=${R1%_R1*}
 
 cd /chr4/
 gunzip *.gz
-echo STAR --runThreadN 21 --runMode genomeGenerate \
+echo --runThreadN 21 --runMode genomeGenerate \
      --genomeDir /chr4/ \
      --genomeFastaFiles /chr4/Homo_sapiens.GRCh38.dna.chromosome.4.fa \
      --sjdbGTFfile /chr4/Homo_sapiens.GRCh38.97.chromosome.4.gff3 \
+     --sjdbOverhang 98 \
      --genomeSAindexNbases 12
 
 STAR --runThreadN 21 --runMode genomeGenerate \
      --genomeDir /chr4/ \
      --genomeFastaFiles /chr4/Homo_sapiens.GRCh38.dna.chromosome.4.fa \
      --sjdbGTFfile /chr4/Homo_sapiens.GRCh38.97.chromosome.4.gff3 \
+     --sjdbOverhang 98 \
      --genomeSAindexNbases 12
 
-mkdir -p /working/star
-mkdir -p ${outfile}
-cd /working/star/
-cd ${outfile}
+
+mkdir -p /working/star/
+cd /working/star
+mkdir -p $outdir
+cd $outdir
 echo STAR --runThreadN 5 --genomeDir /chr4/ \
      --readFilesCommand zcat \
-     --sjdbGTFfile /chr4/Homo_sapiens.GRCh38.97.chromosome.4.gff3 \
      --readFilesIn ${R1} ${R2} \
+     --sjdbGTFfile /chr4/Homo_sapiens.GRCh38.97.chromosome.4.gff3 \
      --quantMode GeneCounts \
      --outSAMtype BAM SortedByCoordinate
+     --sjdbGTFtagExonParentGeneName Name
 
 STAR --runThreadN 5 --genomeDir /chr4/ \
      --readFilesCommand zcat \
-     --sjdbGTFfile /chr4/Homo_sapiens.GRCh38.97.chromosome.4.gff3 \
      --readFilesIn ${R1} ${R2} \
+     --sjdbGTFfile /chr4/Homo_sapiens.GRCh38.97.chromosome.4.gff3 \
      --quantMode GeneCounts \
      --outSAMtype BAM SortedByCoordinate
 
