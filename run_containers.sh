112e3d67d0b2a8a01490b3d5cdc4bf3abd8967a6
urrutia <jurrutia@tacc.utexas.edu>
2019-07-23 11:06:17 -0500

    updating script name

---
+++ run_containers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+R1=${1}
+R2=${2}
+MOUNT="type=bind,source=$PWD,target=/working"
+
+docker run --rm --mount ${MOUNT} docker_compose_pipeline_trimmomatic:latest \
+       /opt/scripts/run_trimmomatic.sh \
+       /working/$R1 /working/$R2 \
+       TruSeq3-PE-2.fa:2:30:10 \
+       36
+
+docker run --rm --mount ${MOUNT} docker_compose_pipeline_star:latest \
+       /opt/scripts/star.sh \
+       /working/trimmomatic/${R1%.f*}.trimmed.fastq.gz \
+       /working/trimmomatic/${R2%.f*}.trimmed.fastq.gz
+
+
+docker run --rm -mount ${MOUNT} docker_compose_pipeline_multiqc:latest \
+       multiqc \
+       -o /working/multiqc/ \
+       /working
